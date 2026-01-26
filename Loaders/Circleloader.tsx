906d9b3a204e789516eeea36f31e6a3dfab76415
Mongus-2064 <mongus2064@gmail.com>
2026-01-26 20:31:39 +0545

    Fixed login issues

--- Loaders/Circleloader.tsx
+++ Loaders/Circleloader.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Circleloader() {
   return (
-    <div className=' inset-0 border-2 w-8 h-8 border-white rounded-full border-t-0 animate-spin'>
+    <div className=' inset-0  border-3 w-8 h-8 border-white rounded-full border-t-0 animate-spin'>
     </div>
   )
 }
